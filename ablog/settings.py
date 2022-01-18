e570918cc3ea4130d3aa20e8d2e5fff915aeb80a
SaberHardy <samirsamado5@gmail.com>
2022-01-18 14:44:18 +0300

    Create login and registration templates

--- ablog/settings.py
+++ ablog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # new Apps
     'blogapp',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,6 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- ablog/urls.py
+++ ablog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blogapp.urls')),
+    path('members/', include('members.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+
     path('admin/', admin.site.urls),
 ]
