a61d1ff21af29b11b2ffb2ad12dbdc12cf7161ee
SaberHardy <samirsamado5@gmail.com>
2022-01-21 20:16:18 +0300

    update password profile

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('registrations/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+
+    # in this path it not working very good so we need to create our own view"""
+    # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
+    # """My new class view""",
+    path('password/', PasswordChangeView.as_view(template_name='registration/change_password.html')),
+    path('password_success/', password_success, name="password_success"),
+
 ]
