ceb8c2ee728da22cfc902d1fb437b379b69fc5fe
SaberHardy <samirsamado5@gmail.com>
2022-01-26 17:39:27 +0300

    add serialzers using(serializers.Serializer)

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.ApiBlogObjects.as_view()),
-    path('api/<int:pk>/', views.BlogObjectsDetail.as_view()),
+    # path('', views.ApiBlogObjects.as_view()),
+    # path('api/<int:pk>/', views.BlogObjectsDetail.as_view()),
 ]
 
 urlpatterns += format_suffix_patterns(urlpatterns)
--- blogapi/views.py
+++ blogapi/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 
-from blogapp.models import Post
-from .serializers import PostSerializer
-
-
-class ApiBlogObjects(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class BlogObjectsDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# from blogapp.models import Post
+# from .serializers import PostSerializer
+#
+#
+# class ApiBlogObjects(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class BlogObjectsDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
